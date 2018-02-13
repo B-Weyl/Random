e7d3a53e8efa837fdb00455e165ec2eaa8744917
Brian Weyl <bweyl@brians-mbp.lan>
2018-02-12 22:06:33 -0500

    refactored code, better variable names

--- news_scraper.py
+++ news_scraper.py
@@ -7,14 +7,14 @@ if response.status_code == 200:
     data = response.text
 
 soup = BeautifulSoup(data, 'html.parser')
-the_urls = []
-the_headlines = []
+urls = []
+post_titles = []
 for link in soup.find_all('a', itemprop='url'):
-    the_urls.append(BASE_URL + link.get('href'))
+    urls.append(BASE_URL + link.get('href'))
 headlines = soup.find_all(itemprop='headline')
-for _ in range(len(headlines)):
-    if headlines[_].text not in the_headlines:
-        the_headlines.append(headlines[_].text)
+for headline in range(len(headlines)):
+    if headlines[headline].text not in the_headlines:
+        post_titles.append(headlines[headline].text)
 titles_and_urls = zip(the_headlines, the_urls)
 for title, url in titles_and_urls:
     print(title + '\n' + url)
