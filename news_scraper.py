85613eb4de3ff06bc98883e737b5eb80abd7a823
Brian Weyl <bweyl@Brians-MacBook-Pro.local>
2018-09-26 09:14:35 -0400

    Updated news scraper to work with site changes

--- news_scraper.py
+++ news_scraper.py
@@ -1,20 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'http://www.delawareonline.com'
-response = requests.get("http://www.delawareonline.com/news/crime")
+BASE_URL = 'https://www.delawareonline.com'
+response = requests.get("https://www.delawareonline.com/news/crime")
 if response.status_code == 200:
     data = response.text
-
+# print(data)
 soup = BeautifulSoup(data, 'html.parser')
-urls = []
+the_urls = []
 post_titles = []
-for link in soup.find_all('a', itemprop='url'):
-    urls.append(BASE_URL + link.get('href'))
-headlines = soup.find_all(itemprop='headline')
-for headline in range(len(headlines)):
-    if headlines[headline].text not in the_headlines:
-        post_titles.append(headlines[headline].text)
-titles_and_urls = zip(the_headlines, the_urls)
+for link in soup.find_all('a', {'class': 'js-asset-link'}):
+    the_urls.append(BASE_URL + link.get('href'))
+the_headlines = soup.find_all('h1', {'class': 'flm-hed'})
+for headline in the_headlines:
+    if headline.text not in post_titles:
+        post_titles.append(headline.text)
+titles_and_urls = zip(post_titles, the_urls)
 for title, url in titles_and_urls:
     print(title + '\n' + url)
