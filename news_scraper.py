743b5dd4de64bfd1cc720503b2829c8a7bb972f5
Brian Weyl <bweyl@brians-mbp.lan>
2018-09-28 10:12:29 -0400

    fixed mismatched article headlines and urls

--- news_scraper.py
+++ news_scraper.py
@@ -8,12 +8,20 @@ if response.status_code == 200:
 soup = BeautifulSoup(data, 'html.parser')
 the_urls = []
 post_titles = []
+post_ages = []
 for link in soup.find_all('a', {'class': 'js-asset-link'}):
     the_urls.append(BASE_URL + link.get('href'))
 the_headlines = soup.find_all('h1', {'class': 'flm-hed'})
+the_dates = soup.find_all('p', {'class': 'flm-time-since-update'})
+for date in the_dates:
+    post_ages.append(' '.join(date.text.split()))
 for headline in the_headlines:
     if headline.text not in post_titles:
         post_titles.append(headline.text)
-titles_and_urls = zip(post_titles, the_urls)
-for title, url in titles_and_urls:
-    print(f"{title}" + '\n' + f"{url}")
+the_urls = the_urls[3:]
+# very hackish because of picking up the three news stories from the top
+# section need to adjust scraping the headlines to match with that..
+# but for now this will have to do
+titles_and_urls_and_ages = zip(post_titles, the_urls, post_ages)
+for title, url, age in titles_and_urls_and_ages:
+    print(f"{title}" + '\n' + f"{url} {age}")
