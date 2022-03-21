739b1100503fb22c088ad9696b77fbbd917d5659
Brian Weyl <btw2292@gmail.com>
2022-03-21 14:00:04 -0400

    updated varaible names in program, updated html fields to correctly get news articles again

--- news_scraper.py
+++ news_scraper.py
@@ -5,7 +5,7 @@ BASE_URL = 'https://www.delawareonline.com'
 
 # code rewritten to utilize functions and main instead of just running as a
 # block of code
-# will leave the previous code commented below for reference.
+
 
 
 def get_content(url):
@@ -17,17 +17,17 @@ def get_content(url):
 
 def parse_data(data):
     soup = BeautifulSoup(data, 'html.parser')
-    the_urls, post_titles, post_ages = [], [], []
-    for link in soup.find_all('a', {'class': 'js-asset-link'}):
-        the_urls.append(BASE_URL + link.get('href'))
-    the_headlines = soup.find_all('span', {'class': 'js-asset-headline'})
-    the_dates = soup.find_all('p', {'class': 'flm-time-since-update'})
-    for date in the_dates:
+    post_urls, post_titles, post_ages = [], [], []
+    for link in soup.find_all('a', {'class': 'gnt_m_flm_a'}):
+        post_urls.append(BASE_URL + link.get('href'))
+    headlines = soup.find_all('a', {'class': 'gnt_m_flm_a'})
+    dates = soup.find_all('div', {'class': 'gnt_m_flm_sbt'})
+    for date in dates:
         post_ages.append(' '.join(date.text.split()))
-    for headline in the_headlines:
+    for headline in headlines:
         if headline.text not in post_titles:
             post_titles.append(headline.text)
-    return zip(post_titles, the_urls, post_ages)
+    return zip(post_titles, post_urls, post_ages)
 
 
 def print_nice(data):
@@ -37,33 +37,11 @@ def print_nice(data):
 
 def main():
     site = get_content('https://www.delawareonline.com/news/crime')
-    the_data = parse_data(site)
-    print_nice(the_data)
+    data = parse_data(site)
+    print_nice(data)
 
 
 if __name__ == '__main__':
     main()
 
-# response = requests.get("https://www.delawareonline.com/news/crime")
-# if response.status_code == 200:
-#     data = response.text
-# soup = BeautifulSoup(data, 'html.parser')
-# the_urls = []
-# post_titles = []
-# post_ages = []
-# for link in soup.find_all('a', {'class': 'js-asset-link'}):
-#     the_urls.append(BASE_URL + link.get('href'))
-# the_headlines = soup.find_all('span', {'class': 'js-asset-headline'})
-# the_dates = soup.find_all('p', {'class': 'flm-time-since-update'})
-# for date in the_dates:
-#     post_ages.append(' '.join(date.text.split()))
-# for headline in the_headlines:
-#     if headline.text not in post_titles:
-#         post_titles.append(headline.text)
-# # the_urls = the_urls[3:]
-# # very hackish because of picking up the three news stories from the top
-# # section need to adjust scraping the headlines to match with that..
-# # but for now this will have to do
-# titles_and_urls_and_ages = zip(post_titles, the_urls, post_ages)
-# for title, url, age in titles_and_urls_and_ages:
-#     print(f"{title} " + f" {age}" + '\n' + f"{url}")
+
